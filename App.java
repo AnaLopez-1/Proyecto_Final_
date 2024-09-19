079de4de349aea4425120ab71b305680c2531365
Ana_Lopez <dianalareiza2@gmail.com>
2024-09-19 10:49:19 -0500

    ProyectoFinal

---
+++ App.java
@@ -0,0 +1,11 @@
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+        //CREACIÓN DE CONCESIONARIO
+        Concesionario concesionario = new Concesionario("CarMotion");
+    }
+}
--- Concesionario.java
+++ Concesionario.java
@@ -1,3 +1,21 @@
 public class Concesionario {
-    
+    private String nombre;
+
+    public Concesionario(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Concesionario\n"+
+        "Nombre:" + nombre;
+    }
 }
