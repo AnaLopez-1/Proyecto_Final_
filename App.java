663e0190625bffb1da84c64d11c9569c3ad50ce0
Ana_Lopez <dianalareiza2@gmail.com>
2024-09-21 00:33:51 -0500

    ProyectoFinal

--- App.java
+++ App.java
@@ -7,7 +7,22 @@ public class App {
 
         //CREACIÓN DE CONCESIONARIO
         Concesionario concesionario = new Concesionario("CarMotion");
-
-        
+    
+        // Crear Vehiculo
+        Vehiculo vehiculo = new Vehiculo("Toyota", true, 2022, "Automático", 180, "2.0L");
+        System.out.println("Detalles del Vehículo:");
+        System.out.println(vehiculo.toString());
+        System.out.println();
+    
+        // Crear Buses
+        Buses bus = new Buses("Mercedes-Benz", false, 2018, "Manual", 120, "6.0L", 50, 2, "2000L", 8);
+        System.out.println("Detalles del Bus:");
+        System.out.println(bus.toString());
+        System.out.println();
+    
+        // Crear Vans
+        Vans van = new Vans("Ford", true, 2023, "Automático", 160, "3.5L", 12, 4, "800L", 4);
+        System.out.println("Detalles de la Van:");
+        System.out.println(van.toString());
+        }
     }
-}
